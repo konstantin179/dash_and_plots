d328354f49d5ebb8f3a423a33f17e1d2f8fe1feb
Konstantin Lobov <kostialobov@gmail.com>
2022-12-02 19:19:41 +0300

    added dwm selection, orders revenue sum, orders count, aiv, bar, line graphs.

--- dash_app.py
+++ dash_app.py
@@ -6,7 +6,6 @@ import os
 from dotenv import load_dotenv
 from postgres import DB, db_connection_string
 
-
 load_dotenv()
 # params = {"Accept": "application/json",
 #           "api_id": os.getenv("API_ID"),
@@ -49,37 +48,223 @@ app.layout = html.Div([
             first_day_of_week=1,
             clearable=True,
             show_outside_days=True,
-            ),
-        ]
+        ),
+    ]
     ),
     html.Div(children=[
         html.Label('Продавец'),
         dcc.Dropdown(options=seller_names,
                      value=seller_names[0],
                      id='seller_name_dropdown', ),
-        ],
+    ],
     ),
     html.Div(children=[
         html.Label('Категории'),
         dcc.Dropdown(id='category_dropdown', multi=True, ),
-        ],
+    ],
     ),
     html.Div(children=[
         html.Label('Бренд'),
         dcc.Dropdown(id='brand_dropdown', multi=True, ),
-        ],
+    ],
     ),
     html.Div(children=[
         html.Label('Товар'),
         dcc.Dropdown(id='product_dropdown', multi=True, ),
     ],
     ),
-    # dcc.Graph(id='Graph1'),
-    # dcc.Graph(id='line_plot1'),
-    # dcc.Graph(id='line_plot2'),
+    html.Label('Выбор день, неделя или месяц'),
+    dcc.RadioItems(options=[{'label': 'День', 'value': 'day'},
+                            {'label': 'Неделя', 'value': 'week'},
+                            {'label': 'Месяц', 'value': 'month'},
+                            ],
+                   value='day',
+                   id="d_w_m_selection",
+                   inline=True,
+                   ),
+
+    html.Label('Сумма заказов, руб.'),
+    html.Div([
+        html.Label('Последние 7 дней'),
+        html.Div(id='revenue_seven_days'),
+        html.Label('Предыдущие 7 дней'),
+        html.Div(id='revenue_pre_seven_days'),
+    ],
+    ),
+    html.Label('Количество заказов, шт.'),
+    html.Div([
+        html.Label('Последние 7 дней'),
+        html.Div(id='order_units_seven_days'),
+        html.Label('Предыдущие 7 дней'),
+        html.Div(id='order_units_pre_seven_days'),
+    ],
+    ),
+    html.Label('AIV, руб.'),
+    html.Div([
+        html.Label('Последние 7 дней'),
+        html.Div(id='aiv_seven_days'),
+        html.Label('Предыдущие 7 дней'),
+        html.Div(id='aiv_pre_seven_days'),
+    ],
+    ),
+
+    dcc.Graph(id='revenue_bar'),
+    html.Div(children=[
+        dcc.Graph(id='impressions_to_cart_conversion'),
+        dcc.Graph(id='cart_to_order_conversion'),
+        dcc.Graph(id='impressions_to_order_conversion'),
+    ],
+    ),
 
 ])
-# fig = px.bar(long_df, x="nation", y="count", color="medal", title="Long-Form Input")
+
+
+def get_params_lists(seller_name, category_names, all_category_names,
+                     brand_names, all_brand_names, sku_names, all_sku_names):
+    api_id = df.loc[df["api_name"] == seller_name]["api_id"].iloc[0]
+    api_id_list = [api_id, ]
+    if "Выбрать все" in category_names:
+        all_category_names.remove("Выбрать все")
+        category_name_list = all_category_names
+    else:
+        category_name_list = category_names
+    if "Выбрать все" in brand_names:
+        all_brand_names.remove("Выбрать все")
+        brand_name_list = all_brand_names
+    else:
+        brand_name_list = brand_names
+    if "Выбрать все" in sku_names:
+        all_sku_names.remove("Выбрать все")
+        sku_name_list = all_sku_names
+    else:
+        sku_name_list = sku_names
+    params = {"api_id_list": api_id_list,
+              "brand_name_list": brand_name_list,
+              "category_name_list": category_name_list,
+              "sku_name_list": sku_name_list,
+              }
+    return params
+
+
+@app.callback(
+    Output('revenue_seven_days', 'children'),
+    Output('revenue_pre_seven_days', 'children'),
+    Output('order_units_seven_days', 'children'),
+    Output('order_units_pre_seven_days', 'children'),
+    Output('aiv_seven_days', 'children'),
+    Output('aiv_pre_seven_days', 'children'),
+    Input('seller_name_dropdown', 'value'),
+    Input('category_dropdown', 'value'),
+    Input('category_dropdown', 'options'),
+    Input('brand_dropdown', 'value'),
+    Input('brand_dropdown', 'options'),
+    Input('product_dropdown', 'value'),
+    Input('product_dropdown', 'options'), )
+def values_seven_n_pre_seven_days(seller_name, category_names, all_category_names,
+                                  brand_names, all_brand_names, sku_names, all_sku_names):
+    if not (category_names and all_category_names and brand_names
+            and all_brand_names and sku_names and all_sku_names):
+        return "", "", "", "", "", ""
+    params = get_params_lists(seller_name, category_names, all_category_names,
+                              brand_names, all_brand_names, sku_names, all_sku_names)
+    # print(params)
+    titles = ['revenue_seven_days', 'revenue_pre_seven_days', 'order_units_seven_days',
+              'order_units_pre_seven_days', 'aiv_seven_days', 'aiv_pre_seven_days']
+    titles_n_values = {}
+    for title in titles:
+        try:
+            # print(f"try to get data to {title}")
+            req = requests.post(f"http://62.84.124.35:5051/api/v1/dashboard/{title}", json=params)
+            data = req.json()
+            # print(data)
+            if req.status_code == 422:
+                titles_n_values[title] = ""
+            else:
+                titles_n_values[title] = list(data.values())[0]
+        except requests.exceptions.RequestException as e:
+            print(f"Request error to /api/v1/dashboard/{title}: " + str(e))
+            titles_n_values[title] = ""
+    values = [f"{value}" for value in titles_n_values.values()]
+    return values
+
+
+@app.callback(
+    Output('revenue_bar', 'figure'),
+    Input('d_w_m_selection', 'value'),
+    Input('seller_name_dropdown', 'value'),
+    Input('category_dropdown', 'value'),
+    Input('category_dropdown', 'options'),
+    Input('brand_dropdown', 'value'),
+    Input('brand_dropdown', 'options'), )
+def revenue_bar_plot(d_w_m, seller_name, category_names, all_category_names, brand_names, all_brand_names):
+    # if not (category_names and all_category_names and brand_names and all_brand_names):
+    #     return None
+    # params_lists = get_params_lists(seller_name, category_names, all_category_names,
+    #                                 brand_names, all_brand_names, [], [])
+    # params = {"api_id": params_lists['api_id_list'][0],
+    #           "brand_id": params_lists['brand_name_list'][0],
+    #           "category_id": params_lists['category_name_list'][0],
+    #           }
+    # print("params:", params)
+    # try:
+    #     print(f"try to get data to revenue_bar_plot")
+    #     req = requests.get(f"http://62.84.124.35:5051/api/v1/day/graphs/revenue_rub", params=params)
+    #     print(req.status_code)
+    #     print(req.content)
+    #     data = req.json()
+    #     print(data)
+    #     if req.status_code == 422:
+    #         dff = pd.DataFrame.from_dict(data)
+    #     else:
+    #         return None
+    # except requests.exceptions.RequestException as e:
+    #     print(f"Request error to /api/v1/{d_w_m}/graphs/revenue_rub: " + str(e))
+    #     return None
+    dff = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
+    fig = px.bar(dff, x="col1", y="col2", title="Динамика суммы заказов")
+    return fig
+
+
+@app.callback(
+    Output('impressions_to_cart_conversion', 'figure'),
+    Input('d_w_m_selection', 'value'),
+    Input('seller_name_dropdown', 'value'),
+    Input('category_dropdown', 'value'),
+    Input('category_dropdown', 'options'),
+    Input('brand_dropdown', 'value'),
+    Input('brand_dropdown', 'options'), )
+def revenue_bar_plot(d_w_m, seller_name, category_names, all_category_names, brand_names, all_brand_names):
+    dff = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
+    fig = px.line(dff, x="col1", y="col2", title="Конверсия из показа в корзину, %")
+    return fig
+
+
+@app.callback(
+    Output('cart_to_order_conversion', 'figure'),
+    Input('d_w_m_selection', 'value'),
+    Input('seller_name_dropdown', 'value'),
+    Input('category_dropdown', 'value'),
+    Input('category_dropdown', 'options'),
+    Input('brand_dropdown', 'value'),
+    Input('brand_dropdown', 'options'), )
+def revenue_bar_plot(d_w_m, seller_name, category_names, all_category_names, brand_names, all_brand_names):
+    dff = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
+    fig = px.line(dff, x="col1", y="col2", title="Конверсия из корзины в заказ, %")
+    return fig
+
+
+@app.callback(
+    Output('impressions_to_order_conversion', 'figure'),
+    Input('d_w_m_selection', 'value'),
+    Input('seller_name_dropdown', 'value'),
+    Input('category_dropdown', 'value'),
+    Input('category_dropdown', 'options'),
+    Input('brand_dropdown', 'value'),
+    Input('brand_dropdown', 'options'), )
+def revenue_bar_plot(d_w_m, seller_name, category_names, all_category_names, brand_names, all_brand_names):
+    dff = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
+    fig = px.line(dff, x="col1", y="col2", title="Конверсия из показа в заказ, %")
+    return fig
 
 
 @app.callback(
